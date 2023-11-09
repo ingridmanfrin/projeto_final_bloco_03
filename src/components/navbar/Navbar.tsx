08cf247e97ef1b52c5ba2dbbb15218938e24b231
ingridmanfrin <ingridevelyn.manfrin@gmail.com>
2023-11-09 17:05:03 -0300

    Adicionado rotas no navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ function Navbar() {
                 <div className="container flex justify-between text-lg">
                     <Link to='/home' className='hover:underline'>Farmácia Mais Saúde</Link>
 
+
                     <div className='flex gap-4'>
                         <Link to='/categorias' className='hover:underline'>Categorias</Link>
                         <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>
